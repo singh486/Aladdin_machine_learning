161455577a09c876a9edcd13a0ab5dbec18c436a
Jasmine Singh <singh486@purdue.edu>
2021-03-31 14:42:59 -0400

    finish verify_json.py tool

--- verify_json.py
+++ verify_json.py
@@ -1,35 +1,46 @@
 import json
 import os
 import sys
-from os.path import isfile,join
+from os.path import isfile, join
 
-# check if a folder name was specified
+# check if a rootdir name was specified
 if len(sys.argv) > 1:
-    folder = sys.argv[1]
+    rootdir = sys.argv[1]
 else:
-    folder = os.getcwd()
+    # otherwise use current directory
+    rootdir = os.getcwd()
 
 # array to hold invalid and valid files
 invalid_json_files = []
 read_json_files = []
 
+
 def parse():
-    # loop through the folder
-    for files in os.listdir(folder):
-        # check if the combined path and filename is a file
-        if isfile(join(folder,files)):
-            # open the file
-            with open(join(folder,files)) as json_file:
-                # try reading the json file using the json interpreter
-                try:
-                    json.load(json_file)
-                    read_json_files.append(files)
-                except ValueError as e:
-                    # if the file is not valid, print the error 
-                    #  and add the file to the list of invalid files
-                    print("JSON object issue: %s" % e)
-                    invalid_json_files.append(files)
-    print(invalid_json_files)
-    print(len(read_json_files))
-    
+    # loop through the rootdir
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            # check if the combined path and filename is a json file
+            if isfile(os.path.join(subdir, file)) and os.path.join(subdir, file)[-5:] == '.json':
+                # open the file
+                with open(os.path.join(subdir, file)) as json_file:
+                    # try reading the json file using the json interpreter
+                    try:
+                        json.load(json_file)
+                        read_json_files.append(os.path.join(subdir, file))
+                    except ValueError as e:
+                        # if the file is not valid, print the error
+                        #  and add the file to the list of invalid files
+                        invalid_json_files.append(os.path.join(
+                            subdir, file) + "\nJSON object issue: %s\n" % e)
+
+    # sort the invalid json files alphabetically
+    invalid_json_files.sort()
+    for invalid in invalid_json_files:
+        print(invalid)
+
+    # print all read json files
+    print("All read json files:")
+    for read in read_json_files:
+        print(read)
+
 parse()
